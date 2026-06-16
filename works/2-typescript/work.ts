f7616088d5a60da484c9c12d7902c8a605d6aaa5
A Watanabe <watanabe_akihiko@care-con.co.jp>
2026-06-16 17:33:39 +0900

    add:0616/TypeScript_Work提出

--- works/2-typescript/work.ts
+++ works/2-typescript/work.ts
@@ -1,4 +1,4 @@
-//[タスク１]Userインターフェイスを定義してください
+//[タスク1]
 // id(数値), name(文字列), email(文字列), role("admin"または"user"のユニオン型)
 interface User {
     id: number;
@@ -6,34 +6,31 @@ interface User {
     email: string;
     role: "user" | "admin";
 }
-//[タスク２]fetchUserの引数、返り値に適切な形を定義してください
-async function fetchUser(id: number): Promise<User | null> {
-    const users = [
+
+//[タスク2]
+const fetchUser = async (id: number): Promise<User | null> => {
+    const users: User[] = [
         { id: 1, name: "田中 太郎", email: "tanaka@example.com", role: "admin" },
         { id: 2, name: "佐藤 次郎", email: "sato@example.com", role: "user" },
     ];
-
     const user = users.find((u) => u.id === id);
-
-    // ユーザーが見つからなかった場合はnullを返す
     return user || null;
 }
 
-//[タスク３]配列を受け取って、その最初の要素を返すジェネリック関数になるように型定義をしてください
-function getFirstElement(array: T[]): T {
+//[タスク3]
+const getFirstElement = <T,>(array: T[]): T => {
     return array[0];
-}
+};
 
 // 実行例
-async function main() {
+const main = async (): Promise<void> => {
     const user = await fetchUser(1);
     if (user) {
         console.log(`名前: ${user.name}, 権限: ${user.role}`);
     }
-
     const numbers = [10, 20, 30];
     const firstNumber = getFirstElement(numbers);
     console.log(`最初の数値: ${firstNumber}`);
-}
+};
 
 main();
