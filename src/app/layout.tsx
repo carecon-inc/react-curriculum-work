74b61cdecffeecb8734cb0ae23afcc10cdf3cfa8
A Watanabe <watanabe_akihiko@care-con.co.jp>
2026-08-19 14:11:52 +0900

    update:0812 トラブルシューティングワーク再提出のため更新

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,18 +2,18 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Simple Message Board",
-  description: "Next.js で作る簡易掲示板アプリ",
+    title: "Simple Message Board",
+    description: "Next.js で作る簡易掲示板アプリ",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="ja">
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="ja">
+            <body>{children}</body>
+        </html>
+    );
 }
